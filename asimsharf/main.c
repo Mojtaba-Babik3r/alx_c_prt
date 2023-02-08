28ffc00a67e6c3ceaca0004af3658486e31d054e
asimsharf <asimsharf@gmail.com>
2023-02-08 07:23:26 +0300

    updated

--- asimsharf/main.c
+++ asimsharf/main.c
@@ -2,12 +2,16 @@
 
 int main(void){
     int age, height, weight;
+
     printf("How old are you? ");
     scanf("%d", &age);
+
     printf("How tall are you? ");   
     scanf("%d", &height);
+
     printf("How much do you weigh? ");
     scanf("%d", &weight);
+    
     printf("So, you're %d old, %d tall and %d heavy. \n", age, height, weight);
     return 0;
 }
